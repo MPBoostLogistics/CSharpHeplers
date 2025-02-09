d21a336f83a402aa0e809378a4840c2f4300c465
vegiwoo <vegiwoo@protonmail.com>
2025-02-09 15:40:57 +0300

    Create Running nUnit tests workflow

--- CSharpHelpers/Models/ScanTextResult.cs
+++ CSharpHelpers/Models/ScanTextResult.cs
@@ -1,40 +1,5 @@
-using System.Globalization;
-using CSharpHelpers.Extensions;
-
 namespace CSharpHelpers.Models
 {
-    public class CreationDocumentDate 
-    {
-        #region Variables and constants
-        private readonly CultureInfo _dateOnlyCultureInfo;
-        #endregion
-
-        #region Properties 
-        public DateOnly Date { get; set; }
-        public string QuarterAndYearString { get; }
-        #endregion
-        
-        public CreationDocumentDate(CultureInfo dateOnlyCultureInfo, string dateOnlyString)
-        {
-            _dateOnlyCultureInfo = dateOnlyCultureInfo;
-            if(DateOnly.TryParse(dateOnlyString, _dateOnlyCultureInfo, out var date)) 
-            {
-                Date = date;
-                QuarterAndYearString = Date.GetQuarterAndYearString();
-            } 
-            else 
-            {
-                throw new FormatException($"String in format '{dateOnlyString}' can't be converted to date.");
-            }
-        }
-    }
-
-    public interface IScanTextResult 
-    {
-        public CreationDocumentDate CreationDate { get; set; }
-    }
-
-
     public class ScanTextResult(string filePath, string text, double scanTime, bool isConvertToPdfRequired)
     {
         #region Properties
