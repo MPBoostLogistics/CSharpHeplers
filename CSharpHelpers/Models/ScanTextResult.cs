8d3eed8ad347c01f929d34c5ef2d53ec65f374df
vegiwoo <vegiwoo@protonmail.com>
2025-02-11 14:42:45 +0300

    Add new name and date in ScanTextResult

--- CSharpHelpers/Models/ScanTextResult.cs
+++ CSharpHelpers/Models/ScanTextResult.cs
@@ -8,8 +8,13 @@ namespace CSharpHelpers.Models
         public string Text { get; private set; } = text;
         public double ScanTime { get; private set; } = scanTime;
         public bool IsConvertToPdfRequired { get; private set; } = isConvertToPdfRequired;
-
+        public string? NewFileName { get; private set;} = null;
+        public DateOnly? NewFileDate { get; private set;} = null;
         #endregion
 
+        #region Functionality
+        public void Set(string newFileName) => NewFileName = newFileName.Trim();
+        public void Set(DateOnly newFileDate) => NewFileDate = newFileDate;
+        #endregion
     }
 }
