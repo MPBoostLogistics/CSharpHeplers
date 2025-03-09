a8a84c38f3d20b1589aa931954857f5998d6f7dd
vegiwoo <vegiwoo@protonmail.com>
2025-03-09 13:12:49 +0300

    Work commit

--- CSharpHelpers/obj/Release/net8.0/CSharpHelpers.AssemblyInfo.cs
+++ CSharpHelpers/obj/Release/net8.0/CSharpHelpers.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("bfed26e9-3d70-444b-aa5c-7e8c416f2f88")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MPBoost Logistics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.17.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.17+20d3518eeaa578b26d2f6029a214a7fd439b1eb3")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.19.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.19+736f83838e3fd6b07fa0aa4e16b0a4d71852e4f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpHelpers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpHelpers")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.17.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.19.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
